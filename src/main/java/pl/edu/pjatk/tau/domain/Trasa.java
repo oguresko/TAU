4d5e293399b94e6819c3067f8f7eb5f97fcb777e
s12116 <s12116@lab3l-5.pjwstk.edu.pl>
2017-10-01 12:33:22 +0200

    budowanie testu

--- src/main/java/pl/edu/pjatk/tau/domain/Trasa.java
+++ src/main/java/pl/edu/pjatk/tau/domain/Trasa.java
@@ -12,8 +12,43 @@ import java.util.Date;
  */
 public class Trasa {
     int id;
-    Date start_date;
-    Date end_date;
+    Date startDate;
+    Date endDate;
     double kilometry;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date start_date) {
+        this.startDate = start_date;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date end_date) {
+        this.endDate = end_date;
+    }
+
+    public double getKilometry() {
+        return kilometry;
+    }
+
+    public void setKilometry(double kilometry) {
+        this.kilometry = kilometry;
+    }
+    
     
 }
+
+
