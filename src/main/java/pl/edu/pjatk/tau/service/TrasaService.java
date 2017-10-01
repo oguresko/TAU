2bb4a6eafc342659d015546a4fc10a1930fed46a
s12116 <s12116@lab3l-5.pjwstk.edu.pl>
2017-10-01 11:41:53 +0200

    uzupełnienie TrasaService

--- src/main/java/pl/edu/pjatk/tau/service/TrasaService.java
+++ src/main/java/pl/edu/pjatk/tau/service/TrasaService.java
@@ -4,10 +4,21 @@
  */
 package pl.edu.pjatk.tau.service;
 
+import java.util.List;
+import pl.edu.pjatk.tau.domain.Trasa;
+
 /**
  *
  * @author s12116
  */
 public interface TrasaService {
+    
+    Trasa add(Trasa trasa);
+    
+    Trasa get(int id) throws IllegalArgumentException;
+
+    List<Trasa> getAll();
+    
+    void delete(int id);
     
 }
