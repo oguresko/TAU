0b55bf1ebf59e8876a95c0e92a4b35a23d98c8a4
s12116 <s12116@lab3l-5.pjwstk.edu.pl>
2017-10-01 11:05:26 +0200

    init

---
+++ src/main/java/pl/edu/pjatk/tau/domain/Trasa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.pjatk.tau.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author s12116
+ */
+public class Trasa {
+    int id;
+    Date start_date;
+    Date end_date;
+    double kilometry;
+    
+}
